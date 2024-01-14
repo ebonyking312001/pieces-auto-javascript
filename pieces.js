186629583a6155743fd88a7c2e663c47532a953f
33761 <noe.maurel31@gmail.com>
2024-01-14 13:08:11 +0100

    Add show all pieces-auto with boucle for

--- pieces.js
+++ pieces.js
@@ -1,19 +1,51 @@
 // Récupération des pièces depuis le fichier JSON
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
+//
+//const article = pieces[0];
+//const imageElement = document.createElement("img");
+//imageElement.src = article.image;
+//const nomElement = document.createElement("h2");
+//nomElement.textContent = article.nom;
+//const prixElement = document.createElement("p");
+//prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+//const categorieElement = document.createElement("p");
+//categorieElement.textContent = article.categorie ?? "(aucune catégorie)"
+//
+//const sectionFiches = document.querySelector(".fiches");
+//sectionFiches.appendChild(imageElement);
+//sectionFiches.appendChild(nomElement);
+//sectionFiches.appendChild(prixElement);
+//sectionFiches.appendChild(categorieElement);
 
-const article = pieces[0];
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
-const nomElement = document.createElement("h2");
-nomElement.textContent = article.nom;
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
-const categorieElement = document.createElement("p");
-categorieElement.textContent = article.categorie ?? "(aucune catégorie)"
+for (let i=0; i < pieces.length; i++){
+    const article = pieces[i];
+    const sectionFiches = document.querySelector(".fiches");
+    const pieceElement = document.createElement("article");
 
-const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
+    const imageElement = document.createElement("img");
+    imageElement.src = article.image;
+
+    const nomElement = document.createElement("h2");
+    nomElement.textContent = article.nom;
+
+    const prixElement = document.createElement("p");
+    prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+
+    const categorieElement = document.createElement("p");
+    categorieElement.textContent = article.categorie ?? "(aucune catégorie)";
+
+    const descriptionElement = document.createElement("p");
+    descriptionElement.textContent = article.description ?? "(Pas de description pour le moment)";
+
+    const stockElement = document.createElement("p");
+    stockElement.textContent = article.disponibilite ? "En stock" : "Rupture de stock";
+
+    sectionFiches.appendChild(pieceElement);
+    pieceElement.appendChild(imageElement);
+    pieceElement.appendChild(nomElement);
+    pieceElement.appendChild(prixElement);
+    pieceElement.appendChild(categorieElement);
+    pieceElement.appendChild(descriptionElement);
+    pieceElement.appendChild(stockElement);
+}
