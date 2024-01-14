6917745336b0a19af957b8c648cde0469fb150a4
33761 <noe.maurel31@gmail.com>
2024-01-14 16:20:07 +0100

    Add sort and filter button

--- pieces.js
+++ pieces.js
@@ -1,22 +1,6 @@
 // Récupération des pièces depuis le fichier JSON
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
-//
-//const article = pieces[0];
-//const imageElement = document.createElement("img");
-//imageElement.src = article.image;
-//const nomElement = document.createElement("h2");
-//nomElement.textContent = article.nom;
-//const prixElement = document.createElement("p");
-//prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
-//const categorieElement = document.createElement("p");
-//categorieElement.textContent = article.categorie ?? "(aucune catégorie)"
-//
-//const sectionFiches = document.querySelector(".fiches");
-//sectionFiches.appendChild(imageElement);
-//sectionFiches.appendChild(nomElement);
-//sectionFiches.appendChild(prixElement);
-//sectionFiches.appendChild(categorieElement);
 
 for (let i=0; i < pieces.length; i++){
     const article = pieces[i];
@@ -49,3 +33,41 @@ for (let i=0; i < pieces.length; i++){
     pieceElement.appendChild(descriptionElement);
     pieceElement.appendChild(stockElement);
 }
+
+const boutonTrier = document.querySelector(".btn-trier")
+;
+boutonTrier.addEventListener("click", function (){
+    const piecesOrdonnes = Array.from(pieces);
+    piecesOrdonnes.sort(function(a, b){
+        return a.prix - b.prix;
+    });
+    console.log(piecesOrdonnes);
+});
+
+const boutonFilter = document.querySelector(".btn-filtrer");
+
+boutonFilter.addEventListener("click", function (){
+    const piecesFltrees = pieces.filter(function(piece){
+        return piece.prix<=35;
+    });
+    console.log(piecesFltrees);
+});
+
+const boutonDescription = document.querySelector(".btn-description");
+
+boutonDescription.addEventListener("click", function (){
+    const piecesDescription = pieces.filter(function(piece){
+        return piece.description;
+    });
+    console.log(piecesDescription);
+});
+
+const boutonDecroissant = document.querySelector(".btn-decroissant");
+
+boutonDecroissant.addEventListener("click", function (){
+    const piecesDecroissant = Array.from(pieces);
+    piecesDecroissant.sort(function(a, b){
+        return b.prix - a.prix;
+    });
+    console.log(piecesDecroissant);
+});
