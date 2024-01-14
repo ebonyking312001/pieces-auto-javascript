28c486f6af2c647d347f6e1631d10de898a432de
33761 <noe.maurel31@gmail.com>
2024-01-14 17:46:32 +0100

    Add pieceDispo and pieceAbordable

--- pieces.js
+++ pieces.js
@@ -71,3 +71,44 @@ boutonDecroissant.addEventListener("click", function (){
     });
     console.log(piecesDecroissant);
 });
+
+const noms = pieces.map(piece =>piece.nom);
+for (let i=pieces.length-1; i>=0; i--){
+    if (pieces[i].prix > 35){
+        noms.splice(i,1);
+    }
+}
+console.log(noms);
+
+const nomDispo = pieces.map(piece => piece.nom); 
+for (let i=pieces.length-1; i>=0; i--){
+    if (pieces[i].disponibilite != true){
+        nomDispo.splice(i,1);
+    }
+};
+
+const prixDispo = pieces.map(piece => piece.prix);
+for (let i=pieces.length-1; i>=0; i--){
+    if (pieces[i].disponibilite != true){
+        prixDispo.splice(i,1);
+    }
+};
+
+const abordablesElements = document.createElement('ul');
+
+for (let i=0; i<noms.length; i++){
+    const nomElement = document.createElement('li');
+    nomElement.innerText = noms[i];
+    abordablesElements.appendChild(nomElement);
+}
+
+document.querySelector('.abordables').appendChild(abordablesElements)
+
+const disponiblesElements = document.createElement('ul');
+
+for (let i=0; i<nomDispo.length; i++){
+    const nomPrixElement = document.createElement('li');
+    nomPrixElement.innerText = nomDispo[i] + " - " + prixDispo[i] ;
+    disponiblesElements.appendChild(nomPrixElement);
+}
+document.querySelector('.disponibles').appendChild(disponiblesElements)
